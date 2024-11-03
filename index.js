932d917d78aac0bf923e005231374a7257e99bf9
Bharat <bharat@mobiuso.com>
2024-11-03 18:46:49 +0530

    fix index file export issue

--- index.js
+++ index.js
@@ -10,40 +10,38 @@ import routes from "./src/routes.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const index = express();
+const app = express();
 
 // ## ----------- APP CONFIGURATION BEGINS HERE ----------- ## //
-index.use(serverConfigMiddleware.restrictHttpHostHeaderInjection);
-index.use(serverConfigMiddleware.corsMiddleware());
-index.use(serverConfigMiddleware.setMandatoryHeaders);
-index.use(serverConfigMiddleware.restrictHttpOptionRequest);
-index.use(serverConfigMiddleware.denyAllExceptWAFRequest);
+app.use(serverConfigMiddleware.restrictHttpHostHeaderInjection);
+app.use(serverConfigMiddleware.corsMiddleware());
+app.use(serverConfigMiddleware.setMandatoryHeaders);
+app.use(serverConfigMiddleware.restrictHttpOptionRequest);
+app.use(serverConfigMiddleware.denyAllExceptWAFRequest);
 
-index.use(express.static(path.resolve(__dirname, "..", "/public")));
-index.use(compression());
-index.use(bodyParser.json());
-index.use(bodyParser.urlencoded({extended: true}));
-index.use(bodyParser.json({limit: "50mb", extended: true}));
+app.use(express.static(path.resolve(__dirname, "..", "/public")));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({limit: "50mb", extended: true}));
 
-index.use(serverConfigMiddleware.preventXSSFilter()); // Prevent XSS-Filter
-index.use(serverConfigMiddleware.preventXFrameOptions()); // Prevent X-Frame-Options
-index.use(serverConfigMiddleware.setCsp()); // Prevent Insecure Content Security Policy (CSP)
-index.use(serverConfigMiddleware.hidePoweredBy());
-index.use(serverConfigMiddleware.setCrossDomainPolicies()); // Prevent Unset/Insecure X-Permitted-Cross-Domain-Policies Header
-index.use(serverConfigMiddleware.setHsts());
+app.use(serverConfigMiddleware.preventXSSFilter()); // Prevent XSS-Filter
+app.use(serverConfigMiddleware.preventXFrameOptions()); // Prevent X-Frame-Options
+app.use(serverConfigMiddleware.setCsp()); // Prevent Insecure Content Security Policy (CSP)
+app.use(serverConfigMiddleware.hidePoweredBy());
+app.use(serverConfigMiddleware.setCrossDomainPolicies()); // Prevent Unset/Insecure X-Permitted-Cross-Domain-Policies Header
+app.use(serverConfigMiddleware.setHsts());
 
-index.use(loggerMiddleware);
+app.use(loggerMiddleware);
 // ## ----------- APP CONFIGURATION ENDS HERE ----------- ## //
 
 // routes
-index.use(routes);
+app.use(routes);
 
 const PORT = process.env.PORT || 8080;
-// import { index } from "./index.js";
+// import { app } from "./app.js";
 
 
-index.listen(PORT, function () {
+app.listen(PORT, function () {
     console.log("Server is listening on port ", PORT);
 });
-
-export {index};
